bf6d31a725be61fad495afe994d4f4d295359755
chamisarobert <chamisa.robert@gmail.com>
2022-01-10 19:50:33 +0200

    Adding Custom Validators to the serializers

--- flightApp/serializers.py
+++ flightApp/serializers.py
@@ -1,10 +1,25 @@
 from rest_framework import serializers
 from flightApp.models import Flight, Passenger, Reservation
+import re
 
 class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = '__all__'
+    # CUSTOM Validators: WAY ONE:
+    # Convention for custom validators: validate_<model_field_name>
+    def validate_flightNumber(self, flightNumber):
+        print('>>>> validate_flightNumber()')
+        if (re.match('^[a-zA-Z0-9]*$', flightNumber)==None):
+            raise serializers.ValidationError('Invalid Flight Number. Please make it alpha-numeric')
+        return flightNumber
+
+    # CUSTOM Validators: WAY TWO:
+    def validate(self, data):
+        print('>>>> validate()')
+        if (re.match('^[a-zA-Z0-9]*$', data['flightNumber'])==None):
+            raise serializers.ValidationError('Invalid Flight Number. Please make it alpha-numeric')
+
 
 class PassengerSerializer(serializers.ModelSerializer):
     class Meta:
