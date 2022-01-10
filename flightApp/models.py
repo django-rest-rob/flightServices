284e4b2079fcf78b2d2f8314aa7728802cba92bb
chamisarobert <chamisa.robert@gmail.com>
2022-01-10 20:33:08 +0200

    Automating Token creation on new user creation

--- flightApp/models.py
+++ flightApp/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+
 # Create your models here.
 class Flight(models.Model):
     flightNumber = models.CharField(max_length=10)
@@ -19,3 +24,30 @@ class Passenger(models.Model):
 class Reservation(models.Model):
     flight = models.ForeignKey(Flight,on_delete=models.CASCADE)
     passenger = models.OneToOneField(Passenger,on_delete=models.CASCADE)
+
+# function to receive signal whenever User is created in the db
+@receiver(post_save,sender=settings.AUTH_USER_MODEL)
+def createAuthToken(sender, instance, created, **kwargs):
+    if created:
+        print(">>>> creating auth_token...")
+        Token.objects.create(user=instance)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
